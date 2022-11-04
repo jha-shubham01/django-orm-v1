1beab1906b090800cf7e6670131d355fc9d74a2c
Shubham Jha <s.jha7777@gmail.com>
2022-11-04 22:05:59 +0530

    Lesson 1: Saving data in Django ORM

--- src/demo/urls.py
+++ src/demo/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register("category", views.CategoryViewSet, basename="category")
+router.register("categories", views.CategoryViewSet, basename="category")
 router.register("comments", views.CommentViewSet, basename="comments")
 router.register("likes", views.LikeViewSet, basename="likes")
 router.register("posts", views.PostViewSet, basename="posts")
--- src/demo/views.py
+++ src/demo/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Category, Comment, Like, Post
 from .serializers import (
@@ -13,6 +15,84 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    @action(detail=False, methods=["POST"])
+    def create_data(self, request, *args, **kwargs):
+        data = self.serializer_class(data=request.data or None)
+        data.is_valid(raise_exception=True)
+
+        title = data.validated_data.get("title")
+        slug = data.validated_data.get("slug")
+        description = data.validated_data.get("description")
+
+        obj = Category.objects.create(
+            title=title, slug=slug, description=description
+        )
+        serializer = self.serializer_class(obj)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=["POST"])
+    def save_data(self, request, *args, **kwargs):
+        data = self.serializer_class(data=request.data or None)
+        data.is_valid(raise_exception=True)
+
+        title = data.validated_data.get("title")
+        slug = data.validated_data.get("slug")
+        description = data.validated_data.get("description")
+
+        obj = Category()
+        obj.title = title
+        obj.slug = slug
+        obj.description = description
+        obj.save()
+
+        serializer = self.serializer_class(obj)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=["POST"])
+    def get_or_create_data(self, request, *args, **kwargs):
+        data = self.serializer_class(data=request.data or None)
+        data.is_valid(raise_exception=True)
+
+        title = data.validated_data.get("title")
+        slug = data.validated_data.get("slug")
+
+        obj, _ = Category.objects.get_or_create(title=title, slug=slug)
+
+        serializer = self.serializer_class(obj)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=["POST"])
+    def bulk_create(self, request, *args, **kwargs):
+        data = self.serializer_class(data=request.data or None, many=True)
+        data.is_valid(raise_exception=True)
+
+        # Inefficient
+        # for row in data.validated_data:
+        #     Category.objects.create(
+        #       title=row["title"], slug=row["slug"], description=row["description"]
+        # )
+
+        # Efficient
+        new_data = []
+        for row in data.validated_data:
+            new_data.append(
+                Category(
+                    title=row["title"],
+                    slug=row["slug"],
+                    description=row["description"],
+                )
+            )
+
+        if new_data:
+            new_data = Category.objects.bulk_create(new_data)
+
+        return Response(
+            "Successfully created data.", status=status.HTTP_201_CREATED
+        )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
